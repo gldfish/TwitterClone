14f2a93036f5d90cb14ee471794d077ab2885aae
grgttdln <georgettedalen@gmail.com>
2024-02-16 16:08:12 +0800

    Added Logo and Branding to website

---
+++ TwitterCloneUi/src/scripts/trending.js
@@ -0,0 +1,32 @@
+
+
+// // https://snowcait.github.io/twitter-swagger-ui
+
+// function getTrend() {
+
+//     var fetchRequest = {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         redirect: 'follow',
+//     };
+
+//     fetch(`https://api.twitter.com/2/trends/by/woeid/:woeid`, fetchRequest)
+//         .then(response => response.text())
+//         .then(result => {
+//             console.log(result);
+//             // After successful registration, save user
+            
+
+//             // Proceed with other actions
+
+            
+
+//             //window.location.href = 'index.html';
+//         })
+//         .catch(error => console.log('error', error)
+//     );
+// }
+
+// getTrend()
